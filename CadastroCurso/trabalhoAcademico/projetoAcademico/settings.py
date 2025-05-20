4f508ce814363b327380015de6f9b7387e9f00d8
Juan Ribeiro <juannopb@gmail.com>
2025-05-20 07:05:02 -0300

    Update settings.py

--- CadastroCurso/trabalhoAcademico/projetoAcademico/settings.py
+++ CadastroCurso/trabalhoAcademico/projetoAcademico/settings.py
@@ -116,5 +116,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'juanribeiro@sou.faccat.br'
-EMAIL_HOST_PASSWORD = 'ngik cani fsxc qxtv'
